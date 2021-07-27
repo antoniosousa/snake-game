2eaccbb016b908c4feea4b87517109c2ec8c5438
antonioufpi <antonioufpi@gmail.com>
2021-07-27 20:07:30 -0300

    regra de gamer over caso atinja seu proprio corpo

--- script.js
+++ script.js
@@ -45,11 +45,18 @@ function atualizar(event){
 }
 
 function iniciarGamer(){
-	
+
 	if(snake[0].x > 15*box && direcao ==="right") snake[0].x=0;
 	if(snake[0].x < 0 && direcao ==="left") snake[0].x=16*box;
 	if(snake[0].y > 15*box && direcao ==="up") snake[0].y=0;
 	if(snake[0].y < 0 && direcao ==="down") snake[0].y=16*box;
+
+	for(i=1;i<snake.length;i++){
+		if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+			clearInterval(game);
+			alert("Gamer Over");
+		}
+	}
 
 	criarBackGround();
 	criarSnake();
